60820343448ff6ccda76babf32429c657e0f7aca
GalinaSherbak84 <galinasherbak@gmail.com>
2021-04-13 17:41:22 +0200

    Stampare le proprietà dell'oggetto.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    
+    //Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+    var studente = {
+        nome: 'Mario',
+        cognome: 'Rossi',
+        eta: '20 anni',
+    }
+    console.log(studente);
+    //Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+    for(var key in studente){
+        console.log(studente[key]);
+    }
 })
