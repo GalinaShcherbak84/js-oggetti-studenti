d77c77c5c06a0a391b3d13d7673a401b6adc54dc
GalinaSherbak84 <galinasherbak@gmail.com>
2021-04-13 18:39:56 +0200

    stampare per ogni studente nome e cognome

--- script.js
+++ script.js
@@ -10,4 +10,33 @@ $(document).ready(function(){
     for(var key in studente){
         console.log(studente[key]);
     }
+    //Creare un array di oggetti di studenti.
+    var studenti =[
+        {
+            nome: 'Mario',
+            cognome: 'Rossi',
+            eta: '20 anni',
+        },
+        {
+            nome: 'Luca',
+            cognome: 'Verdi',
+            eta: '21 anni',
+        },
+        {
+            nome: 'Marco',
+            cognome: 'Neri',
+            eta: '19 anni',
+        },
+        {
+            nome: 'Paolo',
+            cognome: 'Gialli',
+            eta: '22 anni',
+        },
+    ]
+    console.log(studenti);
+    //Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+    for(var i = 0; i < studenti.length; i++){
+        //console.log(studenti[i]);
+        console.log(studenti[i].nome + ' ' + studenti[i].cognome);
+    }
 })
